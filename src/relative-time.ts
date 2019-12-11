4a81bf84bf78afcd792fe269f9c7c199ae7919df
avi tshuva <dharmax@gmail.com>
2019-12-11 16:24:05 +0200

    finished...

--- src/relative-time.ts
+++ src/relative-time.ts
@@ -4,7 +4,7 @@
  * @param t1 either the time or the base time
  * @param t2 if not given, it assumes base time is present time.
  * @param gross - either more accurate or more gross
- * @param language
+ * @param language (currently english only)
  */
 export function relativeTime(t1: Date, t2?: Date, gross = true, language = 'en') {
     if (!t2) {
